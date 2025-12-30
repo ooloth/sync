60604c3f421a4f9821a4351e652b767b578ae9ca
Michael Uloth <hello@michaeluloth.com>
2025-12-30 16:43:19 -0500

    jobs: add sync_likes and sync_subs modules

---
+++ youtube_sync/jobs/sync_likes.py
@@ -0,0 +1,6 @@
+def main():
+    print("Syncing YouTube likes to Notion...")
+
+
+if __name__ == "__main__":
+    main()
---
+++ youtube_sync/jobs/sync_subs.py
@@ -0,0 +1,6 @@
+def main():
+    print("Syncing YouTube subscriptions to Feedbin...")
+
+
+if __name__ == "__main__":
+    main()
