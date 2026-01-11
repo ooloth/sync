f7d3929e29cf8bc8a1c83201522375b6384d7582
Michael Uloth <hello@michaeluloth.com>
2026-01-10 23:42:58 -0500

    chore: delete root level main entrypoint

--- youtube_sync/jobs/sync_subs.py
+++ youtube_sync/jobs/sync_subs.py
@@ -1,5 +1,17 @@
+import rich
+
+from youtube_sync.io.feedbin.client import create_client
+
+
 def main():
-    print("Syncing YouTube subscriptions to Feedbin...")
+    rich.print("Syncing YouTube subscriptions to Feedbin...")
+
+    rich.print("Fetching Feedbin subscriptions")
+    feedbin = create_client()
+    subs = feedbin.list_subscriptions()
+
+    count = len(subs)
+    rich.print(f"Total subscriptions: {count}")
 
 
 if __name__ == "__main__":
